23ce51584eb73305f4befed741b8258c8993f095
Mersal-Mohamed <mersal.mohamed@creiden.com>
2022-02-13 00:06:04 +0400

    push environments files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  googleMapApi: 'put your google map api key',
+  xRapidapiHost: 'put your x rapid key',
+  xRapidapiKey: 'put your x rapid key'
+};
